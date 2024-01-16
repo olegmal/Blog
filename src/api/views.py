8cb3eeb9fa99cb9814f53987a0c6f2eb44b049ef
OlegM <olegm.ua@gmail.com>
2024-01-16 11:04:42 +0200

    Added API

--- src/api/views.py
+++ src/api/views.py
@@ -1,19 +1,24 @@
+from rest_framework import viewsets
 from rest_framework.generics import RetrieveAPIView, ListAPIView
 
 from api.serializers import PostSerializer
 from blog.models import Post
 
 
-class PostListView(ListAPIView):
+# class PostListView(ListAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#
+#
+# class PostDetailView(RetrieveAPIView):
+#     serializer_class = PostSerializer
+#
+#     def get_queryset(self):
+#         return Post.objects.all()
+#
+#     def get_object(self):
+#         return Post.objects.filter(post__pk=self.kwargs.get("pk")).first()
+
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-
-class PostDetailView(RetrieveAPIView):
-    serializer_class = PostSerializer
-
-    def get_queryset(self):
-        return Post.objects.all()
-
-    def get_object(self):
-        return Post.objects.filter(post__pk=self.kwargs.get("pk")).first()
