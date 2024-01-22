a14e284bcdb588a2c3af273c9a05c0e5b0e7fe7d
OlegM <olegm.ua@gmail.com>
2024-01-22 18:35:46 +0200

    Added django docker-compose

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -21,7 +21,7 @@ if os.environ.get("GITHUB_WORKFLOW"):
             "NAME": "postgres",
             "USER": "postgres",
             "PASSWORD": "postgres",
-            "HOST": "0.0.0.0",
+            "HOST": "127.0.0.1",
             "PORT": 5432,
         }
     }
