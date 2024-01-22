614dee6af0227644db30321a971425acc3c28cf4
OlegM <olegm.ua@gmail.com>
2024-01-22 17:19:31 +0200

    Added django docker-compose

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -14,23 +14,36 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default":{
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DB"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        # "HOST": os.environ.get("POSTGRES_HOST"),
-        "HOST": "0.0.0.0",
-        # "PORT": os.environ.get("POSTGRES_PORT"),
-        "PORT": 5432,
-    },
-
-    "default_sqlite": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",  # NOQA
+if os.environ.get("GITHUB_WORKFLOW"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "postgres",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "0.0.0.0",
+            "PORT": 5432,
+        }
     }
-}
+
+else:
+
+
+    DATABASES = {
+        "default":{
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": os.environ.get("POSTGRES_HOST"),
+            "PORT": os.environ.get("POSTGRES_PORT"),
+        },
+
+        "default_sqlite": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",  # NOQA
+        }
+    }
 #
 # STATIC_URL = "static/"
 # STATICFILES_DIRS = [BASE_DIR / "static"]
