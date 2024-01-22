34ea07f50b3db635f56d427390f2710a324d5767
OlegM <olegm.ua@gmail.com>
2024-01-22 16:47:03 +0200

    Added django docker-compose

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -20,8 +20,10 @@ DATABASES = {
         "NAME": os.environ.get("POSTGRES_DB"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("POSTGRES_HOST"),
-        "PORT": os.environ.get("POSTGRES_PORT"),
+        # "HOST": os.environ.get("POSTGRES_HOST"),
+        "HOST": "0.0.0.0",
+        # "PORT": os.environ.get("POSTGRES_PORT"),
+        "PORT": 5432,
     },
 
     "default_sqlite": {
